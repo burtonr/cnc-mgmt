7c2ea9c5dd3533b02d357b555b4d5b4e862a5ae2
Burton Rheutan <rheutan7@gmail.com>
2023-11-09 23:01:07 -0600

    WIP Add MaterialUI from example project

--- pages/api/projects/index.js
+++ pages/api/projects/index.js
@@ -5,7 +5,14 @@ export default async function handler(req, res) {
     const collection = await database.collection('projects')
 
     if (req.method === 'POST') {
-        // TODO: Create new project record
+        // TODO: Create new project record and redirect to the newly created project page
+        const data = req.body
+        const result = await collection.insertOne(data)
+
+        if (!result.insertedId)
+            res.status(400).json({ error: 'Unable to store project'})
+        
+        res.redirect(307, `/projects/${data.name}`)
     } else if (req.method === 'GET') {
         // TODO: Get all project meta data
         const results = await collection.find({}).limit(10).toArray()
--- pages/index.js
+++ pages/index.js
@@ -1,41 +1,59 @@
-import Head from 'next/head';
-import Layout, { siteTitle } from '../components/layout';
-import utilStyles from '../styles/utils.module.css';
+import * as React from 'react';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import Link from '../src/Link';
+
+// import Head from 'next/head';
+// import Layout, { siteTitle } from '../components/layout';
+// import utilStyles from '../styles/utils.module.css';
 import { getSortedProjectData } from '../lib/projects';
-import Link from 'next/link';
-import Date from '../components/date';
+// import Link from 'next/link';
+// import Date from '../components/date';
 
 export default function Home({ allProjectsData }) {
   return (
-    <Layout home>
-      <Head>
-        <title>{siteTitle}</title>
-      </Head>
-      <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
-      </section>
-
-      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Projects</h2>
-        <ul className={utilStyles.list}>
-          {allProjectsData.map(({ id, date, title }) => (
-            <li className={utilStyles.listItem} key={id}>
-              <Link href={`/projects/${id}`}>{title}</Link>
-              <br />
-              <small className={utilStyles.lightText}>
-                <Date dateString={date} />
-              </small>
-            </li>
-          ))}
-        </ul>
-      </section>
-
-    </Layout>
+    <Container maxWidth="sm">
+      <Box sx={{ my: 4 }}>
+        <Typography variant="h4" component="h1" gutterBottom>
+          Material UI - Next.js example
+        </Typography>
+        <Link href="/about" color="secondary">
+          Go to the about page
+        </Link>
+      </Box>
+    </Container>
   );
+  // return (
+  //   <Layout home>
+  //     <Head>
+  //       <title>{siteTitle}</title>
+  //     </Head>
+  //     <section className={utilStyles.headingMd}>
+  //       <p>[Your Self Introduction]</p>
+  //       <p>
+  //         (This is a sample website - you’ll be building a site like this on{' '}
+  //         <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+  //       </p>
+  //     </section>
+
+  //     <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+  //       <h2 className={utilStyles.headingLg}>Projects</h2>
+  //       <ul className={utilStyles.list}>
+  //         {allProjectsData.map(({ id, date, title }) => (
+  //           <li className={utilStyles.listItem} key={id}>
+  //             <Link href={`/projects/${id}`}>{title}</Link>
+  //             <br />
+  //             <small className={utilStyles.lightText}>
+  //               <Date dateString={date} />
+  //             </small>
+  //           </li>
+  //         ))}
+  //       </ul>
+  //     </section>
+
+  //   </Layout>
+  // );
 }
 
 // TODO: This is for static generation. Look into "On-Demand Revalidation" which can be triggered manually
